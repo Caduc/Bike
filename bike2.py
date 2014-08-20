ac3d2be5bf5e6d86fc28ae229b66cec602cecdc4
Jonathan McKenzie <jonathan.t.mckenzie@gmail.com>
2014-08-19 21:42:40 -0400

    Adding results of code review

--- bike2.py
+++ bike2.py
@@ -1,50 +1,84 @@
 #bike 2
-class BobMadeThis(object):
-	"""docstring for BobMadeThis"""
-	def __init__(self, name):
-		self.name = name
+class BobsBikeFactory(object):
+    """docstring for BobsBikeFactory"""
+    def __init__(self, name):
+        self.name = name
 
-	def Madeby(self):
-		print "%s made this" % (self.name)
-
-class BobWheel(BobMadeThis):
-	"""docstring for Wheel"""
-	def __init__(self, style, wheelweight, wheelcost):
-		self.style = style
-		self.wheelweight = wheelweight
-		self.wheelcost = wheelcost
-
-	def wcost(self):
-		print "the cost of the %s is %s" % (self.style, self.wheelcost) 
-
-class BobFrame(BobMadeThis):
-	"""docstring for BobFrame"""
-	def __init__(self, type, frameweight, framecost):
-		self.type = type
-		self.frameweight = frameweight
-		self.framecost = framecost
-'''
-tabling this idea for a bit
-class BikebyBob(BobMadeThis):
-	"""docstring for BikebyBob"""
-	def __init__(self, bobbike,):
-		self.arg = arg
-
-'''		
-
-def Costbobbike(wheelcost,):
-	pass
-
-Bob_mag_wheel = BobWheel("Mag Wheel", 32, 30)
-Bob_spoke_wheel = BobWheel("Spoke Wheel", 22, 50)
-Bob_solid_wheel = BobWheel("Solid Wheel", 12, 70)
+    def made_by(self):
+        print "%s made this" % (self.name)
 
 
-Bob_Alum_frame = BobWheel("Aluminum Frame", 44, 400)
-Bob_Carbon_frame = BobWheel("Carbon Frame", 24, 700)
-Bob_Steel_frame = BobWheel("Steel Frame", 64, 100)	
+class BobWheel(BobsBikeFactory):
+    """docstring for Wheel"""
+    def __init__(self, style, wheel_weight, wheel_cost):
+        self.style = style
+        self.wheel_weight = wheel_weight
+        self.wheel_cost = wheel_cost
 
-Bob = BobMadeThis("Bobby")
+    def cost(self):
+        print "the cost of the %s is %s" % (self.style, self.wheel_cost) 
 
-Bob.Madeby()
-Bob_mag_wheel.wcost()
+
+class BobFrame(BobsBikeFactory):
+    """docstring for BobFrame"""
+    def __init__(self, type, frame_weight, framecost):
+        self.type = type
+        self.frame_weight = frame_weight
+        self.framecost = framecost
+
+
+# tabling this idea for a bit
+class BikebyBob(BobsBikeFactory):
+    """docstring for BikebyBob"""
+    def __init__(self, name, wheel, frame):
+        self.name = name
+        self.weight = (wheel.wheel_weight * 2) + (frame.frame_weight)
+        self.cost = (wheel.wheel_cost * 2) + (frame.frame_cost)
+        # self.wheel = wheel
+        # self.frame = frame
+
+    # def get_weight(self):
+    #     total_weight = (self.wheel.wheel_weight * 2) + (frame.frame_weight)
+    #     return total_weight
+
+
+bob_mag_wheel = BobWheel("Mag Wheel", 32, 30)
+bob_spoke_wheel = BobWheel("Spoke Wheel", 22, 50)
+bob_solid_wheel = BobWheel("Solid Wheel", 12, 70)
+
+
+bob_alum_frame = BobFrame("Aluminum Frame", 44, 400)
+bob_carbon_frame = BobFrame("Carbon Frame", 24, 700)
+bob_steel_frame = BobFrame("Steel Frame", 64, 100)	
+
+
+def total_frame_weight(frames):
+    """
+    Returns the total weight of all frames.
+    frames should be a list of BobFrame
+    e.g. total_frame_weight([bob_alum_frame, bob_carbon_frame, bob_steel_frame])
+    """
+    total_weight = 0
+    for frame in frames:
+        total_weight = total_weight + frame.frameweight 
+    return total_weight
+
+
+def total_weight(things):
+    total_weight = 0
+    for thing in things:
+        total_weight = total_weight + thing.weight 
+    return total_weight
+
+
+Bob = BobsBikeFactory("Bobby")
+
+some_bike = BikebyBob("Cheap Bike", bob_mag_wheel, bob_steel_frame)
+
+if __name__ == "__main__":
+    Bob.made_by()
+    bob_mag_wheel.cost()
+
+    print "Name: %s" % some_bike.name
+    print "Weight: %i" % some_bike.weight
+    print "Cost: %i" % some_bike.cost
---
+++ store.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+import bike2
+
+# maybe define a store
+
+bike2.Bob.made_by()
+
+print "Done"
+
+print "this"
+print "that"
+print "the other thing"
+print customer.name() # name is a method
+print bike_shop.name # name is variable
