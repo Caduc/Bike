2640f426e4a5cccee594f58ee5183bf7bf49da5b
caduc <remaier@gmail.com>
2014-08-30 12:36:57 -0400

    add first line of bike store info to bkstore

--- bkstore.py
+++ bkstore.py
@@ -5,12 +5,15 @@ print "Welcome to the Breaking Away Bike Shop"
 print ""
 
 print "Bikes from Bob's Shop.  Weight is given in pounds"
-print "Name  :{} {} {}" .format(bike2.Bobs_Cheap_Model.name, bike2.Bobs_Mid_Model.name, bike2.Bobs_Exp_Model.name)
-print "Weight:{:^15d}{:^15d}{:^15d}" .format(bike2.Bobs_Cheap_Model.weight, bike2.Bobs_Mid_Model.weight, bike2.Bobs_Exp_Model.weight)
+#print "Name  :{} {} {}" .format(bike2.Bobs_Cheap_Model.name, bike2.Bobs_Mid_Model.name, bike2.Bobs_Exp_Model.name)
+#print "Weight:{:^15d}{:^15d}{:^15d}" .format(bike2.Bobs_Cheap_Model.weight, bike2.Bobs_Mid_Model.weight, bike2.Bobs_Exp_Model.weight)
 print ""
 #print bike2.Ann.made_by()
 print "Bikes from Annies Shop.  Weight is given in pounds"
-print "Name  :{:^15}{:^15}{:^15}" .format(bike2.Anns_Cheap_Model.name, bike2.Anns_Mid_Model.name, bike2.Anns_Exp_Model.name)
-print "Weight:{:^15d}{:^15d}{:^15d}" .format(bike2.Anns_Cheap_Model.weight, bike2.Anns_Mid_Model.weight, bike2.Anns_Exp_Model.weight)
+
+print "{:^15}{:^15}{:^15}" .format("Item Number", "Item Name", "Quantity")
+print "{:^15}{:^15}{:^+15.0f}" .format(bike2.Item1.itemnumber, bike2.Item1.name, bike2.Item1.Quantity)
+#print "Name  :{:^15}{:^15}{:^15}" .format(bike2.Anns_Cheap_Model.name, bike2.Anns_Mid_Model.name, bike2.Anns_Exp_Model.name)
+#print "Weight:{:^15d}{:^15d}{:^15d}" .format(bike2.Anns_Cheap_Model.weight, bike2.Anns_Mid_Model.weight, bike2.Anns_Exp_Model.weight)
 print ""
 print "-------------"
