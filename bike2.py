75c8240257712dc15714ff16be549d76989e3402
caduc <remaier@gmail.com>
2014-08-29 20:48:53 -0400

    replace bobs specific with general reference

--- bike2.py
+++ bike2.py
@@ -1,46 +1,5 @@
 #
-#  Bob's bike factory definition
-#
-class BobsBikeFactory(object):
-    """docstring for BobsBikeFactory"""
-    def __init__(self, name):
-        self.name = name
-
-    def cost(self):
-        print "the cost of the %s is %s" % (self.style, self.wheel_cost) 
-
-    def made_by(self):
-        print "  "
-        print "---- This was proudly made by {} at their factory" .format(self.name)
-
-class BobWheel(BobsBikeFactory):
-    """docstring for Wheel"""
-    def __init__(self, style, wheel_weight, wheel_cost):
-        self.style = style
-        self.wheel_weight = wheel_weight
-        self.wheel_cost = wheel_cost
-
-    def cost(self):
-        print "the cost of the %s is %s" % (self.style, self.wheel_cost) 
-
-class BobFrame(BobsBikeFactory):
-    """docstring for BobFrame"""
-    def __init__(self, type, frame_weight, frame_cost):
-        self.type = type
-        self.frame_weight = frame_weight
-        self.frame_cost = frame_cost
-
-class BobsBikeModel(BobsBikeFactory):
-    """docstring for BobsBikeModel"""
-    def __init__(self, name, wheel, frame):    
-        self.name = name
-        self.weight = (wheel.wheel_weight * 2) + (frame.frame_weight)
-        self.cost = (wheel.wheel_cost * 2) + (frame.frame_cost)
-        self.sell = round(((wheel.wheel_cost * 2) + (frame.frame_cost)) * 1.3)
-        self.shopsell = round((((wheel.wheel_cost * 2) + (frame.frame_cost)) * 1.3) * 1.2)
-
-#
-#  Ann's bike factory definition
+#  General Bike factory definition
 #
 
 class BikeFactory(object):
@@ -107,19 +66,19 @@ def function():
 #
 #  Making bob's bikes
 #
-bob_mag_wheel = BobWheel("Mag Wheel", 32, 30)
-bob_spoke_wheel = BobWheel("Spoke Wheel", 23, 40)
-bob_solid_wheel = BobWheel("Solid Wheel", 12, 70)
+bob_mag_wheel = Wheel("Mag Wheel", 32, 30)
+bob_spoke_wheel = Wheel("Spoke Wheel", 23, 40)
+bob_solid_wheel = Wheel("Solid Wheel", 12, 70)
 
-bob_alum_frame = BobFrame("Aluminum Frame", 44, 180)
-bob_carbon_frame = BobFrame("Carbon Frame", 24, 400)
-bob_steel_frame = BobFrame("Steel Frame", 64, 60)	
+bob_alum_frame = Frame("Aluminum Frame", 44, 180)
+bob_carbon_frame = Frame("Carbon Frame", 24, 400)
+bob_steel_frame = Frame("Steel Frame", 64, 60)	
 
-Bob = BobsBikeFactory("Bobby")
+Bob = BikeFactory("Bobby")
 
-Bobs_Cheap_Model = BobsBikeModel("Cheapy Bike", bob_mag_wheel, bob_steel_frame)
-Bobs_Mid_Model   = BobsBikeModel(" Middy Bike", bob_spoke_wheel, bob_alum_frame)
-Bobs_Exp_Model   = BobsBikeModel("Costly Bike", bob_solid_wheel, bob_carbon_frame)
+Bobs_Cheap_Model = BikeModel("Cheapy Bike", bob_mag_wheel, bob_steel_frame)
+Bobs_Mid_Model   = BikeModel(" Middy Bike", bob_spoke_wheel, bob_alum_frame)
+Bobs_Exp_Model   = BikeModel("Costly Bike", bob_solid_wheel, bob_carbon_frame)
 
 #
 #  Making Ann's bikes
@@ -152,7 +111,7 @@ Angie = Customer("Angie", 1000)
 if __name__ == "__main__":
     Bob.made_by()
 
-    print "Name  :{:^15}{:^15}{:^15}" .format(Bobs_Cheap_Model.name, Bobs_Mid_Model.name, Bobs_Exp_Model.name)
+    print "{:>17}{:^15}{:^15}{:^15}" .format("Name:",Bobs_Cheap_Model.name, Bobs_Mid_Model.name, Bobs_Exp_Model.name)
     print "Weight:{:^15d}{:^15d}{:^15d}" .format(Bobs_Cheap_Model.weight, Bobs_Mid_Model.weight, Bobs_Exp_Model.weight)
     print "Cost  :{:^15d}{:^15d}{:^15d}"      .format(Bobs_Cheap_Model.cost, Bobs_Mid_Model.cost, Bobs_Exp_Model.cost)
     print "Upcost:{:^15.0f}{:^15.0f}{:^15.0f}".format(Bobs_Cheap_Model.sell, Bobs_Mid_Model.sell, Bobs_Exp_Model.sell)
